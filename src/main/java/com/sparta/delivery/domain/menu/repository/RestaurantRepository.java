caffe75a9b951b5ad70f5af5ab9e1bbb338474bb
Anhagu <leejhsam330@gmail.com>
2026-07-06 20:30:23 +0900

    임시 껍데기 RestaurantRepository 생성

---
+++ src/main/java/com/sparta/delivery/domain/menu/repository/RestaurantRepository.java
@@ -0,0 +1,9 @@
+package com.sparta.delivery.domain.menu.repository;
+
+import com.sparta.delivery.domain.restaurant.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, UUID> {
+}
