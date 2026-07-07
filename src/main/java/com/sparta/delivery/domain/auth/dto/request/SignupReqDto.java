5aedbd319495627cbce6fecf4b6dace5731b173c
HanGil <jhg9240@gmail.com>
2026-07-08 00:30:55 +0900

    docs: API 명세에 맞게 username, password 조건 수정

--- src/main/java/com/sparta/delivery/domain/auth/dto/request/SignupReqDto.java
+++ src/main/java/com/sparta/delivery/domain/auth/dto/request/SignupReqDto.java
@@ -11,12 +11,12 @@ import lombok.RequiredArgsConstructor;
 public class SignupReqDto {
 
     @NotBlank(message = "닉네임은 필수입니다.")
-    @Size(min = 2, max = 20, message = "닉네임은 2글자 이상 20글자 이하입니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎ가-힣]+$", message = "닉네임은 영문자, 한글, 숫자만 입력 가능합니다.")
+    @Size(min = 4, max = 10, message = "닉네임은 4글자 이상 10글자 이하입니다.")
+    @Pattern(regexp = "^[a-z0-9]+$", message = "닉네임은 영문소자, 숫자만 입력 가능합니다.")
     private final String username;
 
     @NotBlank(message = "비밀번호는 필수입니다.")
-    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자리 이하입니다.")
+    @Size(min = 8, max = 15, message = "비밀번호는 8자 이상 15자리 이하입니다.")
     @Pattern(regexp = ".*\\d.*", message = "비밀번호는 숫자를 포함해야 합니다.")
     @Pattern(regexp = ".*[a-zA-Z].*", message = "비밀번호는 영문자를 포함해야 합니다.")
     @Pattern(regexp = ".*\\p{Punct}.*", message = "비밀번호는 특수문자를 포함해야 합니다.")
