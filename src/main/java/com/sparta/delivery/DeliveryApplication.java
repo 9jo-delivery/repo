53b126e85e120ccfcf993c94dbebe0c2fd6cbf89
HanGil <jhg9240@gmail.com>
2026-07-07 16:18:09 +0900

    refactor: Spring Security 활성화

--- src/main/java/com/sparta/delivery/DeliveryApplication.java
+++ src/main/java/com/sparta/delivery/DeliveryApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) // 테스트를 위해 exclude = { SecurityAutoConfiguration.class } 하여 Spring Security 자동 적용 해제 추후 제거
+@SpringBootApplication
 public class DeliveryApplication {
 
 	public static void main(String[] args) {
