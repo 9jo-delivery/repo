0b5c245a75dc17e7e2292893a8daba273f855320
Yujun <pathfinder357@github.com>
2026-07-08 12:10:53 +0900

    chore: data.sql 파일을 Git 추적에서 제외

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
--- p_users 테이블의 id는 bigint이므로 1을 직접 넣기 좋습니다.
-INSERT INTO p_users (id, username, password, name, phone, role, is_deleted , created_at)
-VALUES (1, 'test1', 'encoded_password123', '최유준', '010-1111-2222', 'CUSTOMER',false, NOW());
