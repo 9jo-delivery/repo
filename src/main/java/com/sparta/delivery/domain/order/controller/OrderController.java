75514631a3f1a3ed942facfaea4b675617378bcd
Hwan <tmdghks605@naver.com>
2026-07-08 10:33:20 +0900

    chore : 최신화

--- src/main/java/com/sparta/delivery/domain/order/controller/OrderController.java
+++ src/main/java/com/sparta/delivery/domain/order/controller/OrderController.java
@@ -2,11 +2,14 @@ package com.sparta.delivery.domain.order.controller;
 
 import com.sparta.delivery.domain.order.dto.CreatedOrderRequestDto;
 import com.sparta.delivery.domain.order.dto.OrderResponseDto;
+import com.sparta.delivery.domain.order.dto.OrderSummaryResponseDto;
 import com.sparta.delivery.domain.order.service.OrderServiceImpl;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +34,12 @@ public class OrderController {
     }
 
     //주문 목록 검색
-
+//    @GetMapping
+//    public ResponseEntity<List<OrderSummaryResponseDto>> getOrders(
+//            //@AuthenticationPrincipal Long customerId
+//    ){
+//        List<OrderSummaryResponseDto> response = orderService.getOrders(customerId);
+//    }
     //주문 상세 조회
 
     //주문 상태 변경
