78542ca2dfda27ae93496117ec9da069c474cbdf
Hwan <tmdghks605@naver.com>
2026-07-10 13:03:33 +0900

    feat : Payment entity 정리

--- src/main/java/com/sparta/delivery/domain/payment/entity/Payment.java
+++ src/main/java/com/sparta/delivery/domain/payment/entity/Payment.java
@@ -1,17 +1,29 @@
 package com.sparta.delivery.domain.payment.entity;
 
-import jakarta.persistence.*;
-import java.util.UUID;
-import lombok.*;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.SQLRestriction;
-
-import java.time.LocalDateTime;
-
+import com.sparta.delivery.domain.order.entity.Order;
 import com.sparta.delivery.domain.user.entity.User;
 import com.sparta.delivery.global.common.BaseEntity;
 import com.sparta.delivery.global.common.Enums;
-import com.sparta.delivery.domain.order.entity.Order;
+import com.sparta.delivery.global.common.Enums.PaymentMethod;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "p_payments")
@@ -39,21 +51,35 @@ public class Payment extends BaseEntity {
 	private Enums.PaymentMethod paymentMethod = Enums.PaymentMethod.CARD;
 
 	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
+	@Column(name = "payment_status", nullable = false)
 	private Enums.PaymentStatus paymentStatus = Enums.PaymentStatus.READY;
 
 	@Column(nullable = false)
-	private Integer amount;
+	private int amount;
 
+	@Column(name = "paid_at")
 	private LocalDateTime paidAt;
+
+	@Column(name = "cancelled_at")
 	private LocalDateTime cancelledAt;
 
-	@Column(length = 50)
+	@Column(name = "card_company", length = 50)
 	private String cardCompany;
 
-	@Column(length = 30)
+	@Column(name = "card_number_masked", length = 30)
 	private String cardNumberMasked;
 
-	@Column(length = 100)
+	@Column(name = "transaction_id", length = 100)
 	private String transactionId;
+
+	private Payment(Order order, User customer, PaymentMethod paymentMethod, int amount){
+		this.order = order;
+		this.customer = customer;
+		this.paymentMethod = paymentMethod;
+		this.amount = amount;
+	}
+
+	public static Payment create(Order order, User customer, PaymentMethod paymentMethod, int amount){
+		return new Payment(order, customer, paymentMethod, amount);
+	}
 }
