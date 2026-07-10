c3bf934fa930cffc438051e949093ba3acd2933f
Hwan <tmdghks605@naver.com>
2026-07-10 15:58:10 +0900

    feat : Payment 도메인 기본 구조

---
+++ src/main/java/com/sparta/delivery/domain/payment/controller/PaymentController.java
@@ -0,0 +1,17 @@
+package com.sparta.delivery.domain.payment.controller;
+
+import com.sparta.delivery.domain.payment.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payments")
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+
+
+}
