4a096f5b440af0307355d78254117d4a6194bc5c
HanGil <jhg9240@gmail.com>
2026-07-10 10:55:50 +0900

    작업 중이던 AuthService 수정본 저장

--- src/main/java/com/sparta/delivery/domain/auth/service/AuthService.java
+++ src/main/java/com/sparta/delivery/domain/auth/service/AuthService.java
@@ -6,11 +6,10 @@ import com.sparta.delivery.domain.user.entity.User;
 import com.sparta.delivery.domain.user.repository.UserRepository;
 import com.sparta.delivery.global.common.Enums;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -23,10 +22,8 @@ public class AuthService {
     public SignupResDto signup(SignupReqDto reqDto) {
 
         // 중복 유저 검증
-        Optional<User> checkUsername = userRepository.findByUsername(reqDto.getUsername());
-        if (checkUsername.isPresent()) {
-            throw new IllegalArgumentException("이미 존재하는 유저입니다.");
-        }
+        userRepository.findByUsername(reqDto.getUsername()).orElseThrow(()
+                -> new UsernameNotFoundException("Not Found User"));
 
         // 비밀번호 암호화
         String encodedPassword = passwordEncoder.encode(reqDto.getPassword());
