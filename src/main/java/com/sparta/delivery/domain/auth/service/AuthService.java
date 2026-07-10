1f9f7ee72dcd0471423f64cbcd625f2f0adf32b2
HanGil <jhg9240@gmail.com>
2026-07-10 11:05:01 +0900

    fix: 회원가입 중복 유저 검증 오류 수정

--- src/main/java/com/sparta/delivery/domain/auth/service/AuthService.java
+++ src/main/java/com/sparta/delivery/domain/auth/service/AuthService.java
@@ -6,10 +6,11 @@ import com.sparta.delivery.domain.user.entity.User;
 import com.sparta.delivery.domain.user.repository.UserRepository;
 import com.sparta.delivery.global.common.Enums;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +23,10 @@ public class AuthService {
     public SignupResDto signup(SignupReqDto reqDto) {
 
         // 중복 유저 검증
-        userRepository.findByUsername(reqDto.getUsername()).orElseThrow(()
-                -> new UsernameNotFoundException("Not Found User"));
+        Optional<User> checkUsername = userRepository.findByUsername(reqDto.getUsername());
+        if (checkUsername.isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 유저입니다.");
+        }
 
         // 비밀번호 암호화
         String encodedPassword = passwordEncoder.encode(reqDto.getPassword());
