d485b989266a81988474658085de358e18e6a6f2
Yujun <pathfinder357@github.com>
2026-07-14 23:00:39 +0900

    비관적 락 적용 위해 서비스 메서드 로직 수정

--- src/main/java/com/sparta/delivery/domain/review/service/ReviewService.java
+++ src/main/java/com/sparta/delivery/domain/review/service/ReviewService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.sparta.delivery.domain.order.entity.Order;
 import com.sparta.delivery.domain.restaurant.entity.Restaurant;
@@ -20,6 +21,7 @@ import com.sparta.delivery.global.common.Enums;
 
 import com.sparta.delivery.global.exception.ResourceNotFoundException;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -34,7 +36,7 @@ public class ReviewService {
 	private final RestaurantRepository restaurantRepository;
 
 	@Transactional
-	public ReviewResponseDto createReview(UUID orderId, ReviewRequestDto request, Long customerId) {
+	public ReviewResponseDto createReview(UUID orderId, @Valid @RequestBody ReviewRequestDto request, Long customerId) {
 
 		// 주문 검증
 		Order order = orderRepository.findById(orderId)
@@ -60,8 +62,11 @@ public class ReviewService {
 
 		reviewRepository.save(review);
 
-		// dirty Check
-		updateRestaurantRatingAndCount(order.getRestaurant());
+		// 수정 및 삭제와 다르게 생성은 insert 쿼리가 바로 날라가지 않고 쓰기지연 저장소에 있음
+		// 따라서 DB에 강제로 Insert하기 위해서 flush를 호출해서 락을 걸기전에 그 상황을 DB에 insert쿼리를 전송
+		reviewRepository.flush();
+
+		updateRestaurantRatingWithLock(order.getRestaurant().getId());
 
 		return ReviewResponseDto.from(review);
 
@@ -97,8 +102,8 @@ public class ReviewService {
 		// 엔티티 내부의 값을 변경하면, 이 메서드가 끝날 때 JPA가 알아서 UPDATE 쿼리 날림
 		// updatedAt 역시 BaseEntity가 알아서 현재 시간으로 업데이트함..
 		review.updateContentAndRating(request.getContent(),request.getRating());
-
-		updateRestaurantRatingAndCount(review.getRestaurant());
+		// 비관적 락 적용 평점/개수 업데이트 처리
+		updateRestaurantRatingWithLock(review.getRestaurant().getId());
 
 		return ReviewResponseDto.from(review);
 
@@ -113,14 +118,26 @@ public class ReviewService {
 			throw new IllegalArgumentException("자신의 리뷰만 삭제 가능");
 		}
 
-		updateRestaurantRatingAndCount(review.getRestaurant());
 		review.markAsDeleted(customerId); //jpa auditing 객체를 삭제 x 아니고 누가 삭제했는지에 대한 기록
 		// dirty check -> 실제로 commit()이 완료되기 전까지는 삭제됨(아님) 상태임 -> flush()
 		// 커밋이 끝나면 이름표(삭제)를 떼고 실질 데이터베이스에 적용 commit()
+
+		// 식당 Id 외래키 넘겨서 안전하게 비관적 락으로 평점/개수 갱신
+		updateRestaurantRatingWithLock(review.getRestaurant().getId());
+
+
 	}
 
-	private void updateRestaurantRatingAndCount(Restaurant restaurant) {
-		// softDelete 상태?가 아닌 리뷰들의 총갯수 가져옴
+	// 식당 참조 객체를 파라미터로 하지않고 외래키를 받는 이유?
+	// 락을 걸겠다는 명시적 쿼리가 DB로 전송되야함
+	// 객체는 findById로 이미 DB에서 영속성 컨텍스트에 얹어둠
+	// select로 호출됬기 때문에 DB레벨의 Lock은 먹히지않음
+	private void updateRestaurantRatingWithLock(UUID restaurantId) {
+		// select .. for update 쿼리 날라가는 시점
+		// db가 해당 row에 락을 쥐기때문에 동시성 제어가 가능
+		Restaurant restaurant = restaurantRepository.findByIdWithPessimisticLock(restaurantId)
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 가게"));
+
 		Long reviewCount = reviewRepository.countByRestaurantIdAndIsDeletedFalse(restaurant.getId());
 
 		// 찾아온 식당의 리뷰둘의 평균 평점 계산식
