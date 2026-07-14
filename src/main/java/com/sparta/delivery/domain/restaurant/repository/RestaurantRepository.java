b374956252968a534508f080c94103c94c955eed
Yujun <pathfinder357@github.com>
2026-07-14 22:57:45 +0900

    비관적 락 임포트 추가

--- src/main/java/com/sparta/delivery/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/sparta/delivery/domain/restaurant/repository/RestaurantRepository.java
@@ -4,6 +4,7 @@ import com.sparta.delivery.domain.restaurant.entity.Restaurant;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
