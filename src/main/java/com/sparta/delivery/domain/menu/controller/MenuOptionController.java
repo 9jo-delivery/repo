45dfdaa891c578e7bece98013c343eba120ab998
Anhagu <leejhsam330@gmail.com>
2026-07-15 01:30:46 +0900

    feat: 메뉴 옵션 컨트롤러 구현

---
+++ src/main/java/com/sparta/delivery/domain/menu/controller/MenuOptionController.java
@@ -0,0 +1,58 @@
+package com.sparta.delivery.domain.menu.controller;
+
+import com.sparta.delivery.domain.menu.dto.MenuOptionCreateRequest;
+import com.sparta.delivery.domain.menu.dto.MenuOptionCreateResponse;
+import com.sparta.delivery.domain.menu.dto.MenuOptionSearchResponse;
+import com.sparta.delivery.domain.menu.dto.MenuOptionUpdateRequest;
+import com.sparta.delivery.domain.menu.service.MenuOptionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class MenuOptionController {
+
+    public final MenuOptionService menuOptionService;
+    private final Long userId = 1L; // 임시 유저 ID
+
+    @PostMapping("/api/option-groups/{groupId}/options")
+    public ResponseEntity<MenuOptionCreateResponse> createOption(@PathVariable UUID groupId, @Valid @RequestBody MenuOptionCreateRequest request) {
+
+        MenuOptionCreateResponse response = menuOptionService.createMenuOption(groupId, request, userId);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/api/option-groups/{groupId}/options")
+    public ResponseEntity<Page<MenuOptionSearchResponse>> getOptionByGroup(@PathVariable UUID groupId, Pageable pageable) {
+
+        Page<MenuOptionSearchResponse> response = menuOptionService.getOptionByGroup(groupId, pageable);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/api/options/{optionId}")
+    public ResponseEntity<MenuOptionSearchResponse> getOption(@PathVariable UUID optionId) {
+
+        MenuOptionSearchResponse response = menuOptionService.getOptionById(optionId);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PatchMapping("/api/options/{optionId}")
+    public ResponseEntity<MenuOptionSearchResponse> updateOption(@PathVariable UUID optionId, @Valid @RequestBody MenuOptionUpdateRequest request) {
+
+        MenuOptionSearchResponse response = menuOptionService.updateOption(optionId, request, userId);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping("/api/options/{optionId}")
+    public ResponseEntity<Void> deleteOption(@PathVariable UUID optionId) {
+
+        menuOptionService.deleteOption(optionId, userId);
+        return ResponseEntity.noContent().build();
+    }
+}
