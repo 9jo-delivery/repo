d5aefea2cbd8eb83bf8289dbf5e96f9b4b290aab
HanGil <jhg9240@gmail.com>
2026-07-15 10:42:23 +0900

    refactor: GlobalExceptionHandler로 예외 처리

--- src/main/java/com/sparta/delivery/global/config/security/UserDetailsServiceImpl.java
+++ src/main/java/com/sparta/delivery/global/config/security/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.sparta.delivery.global.config.security;
 
 import com.sparta.delivery.domain.user.entity.User;
 import com.sparta.delivery.domain.user.repository.UserRepository;
+import com.sparta.delivery.global.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,8 +17,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user  = userRepository.findByUsername(username).orElseThrow(
-                () -> new UsernameNotFoundException(username + " 님을 찾을 수 없습니다.")
+
+        // UserName 으로 유저 찾기
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new ResourceNotFoundException(username + " 님을 찾을 수 없습니다.")
         );
 
         return new UserDetailsImpl(user);
